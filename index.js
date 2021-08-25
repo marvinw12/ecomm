00f6230a9345ebc34b8cbf737f2555c35dc3fd2a
Marvin W <marvwood12@gmail.com>
2021-08-24 23:11:20 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder='email' />
+            <input name="password" placeholder='password' />
+            <input name="passwordConfirmation" placeholder='password confirmation' />
+            <button>Sign Up</button>
+        </form>
+    </div>
+  `);
+});
+
+const bodyParser = (req, res) => {
+  req.on("data", (data) => {
+    const parsed = data.toString("utf8").split("&");
+    const formData = {};
+    for (let pair of parsed) {
+      const [key, value] = pair.split("=");
+      formData[key] = value;
+    }
+    console.log(formData);
+  });
+};
+
+app.post("/", (req, res) => {
+  res.send("Account created");
+});
+
+app.listen(3000, () => {
+  console.log("listening");
+});
