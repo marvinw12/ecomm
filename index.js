efd73021306a8488ebc514c4027e1b881a9728d9
Marvin W <marvwood12@gmail.com>
2021-08-24 23:34:41 -0400

    Middleware changes

--- index.js
+++ index.js
@@ -8,20 +8,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send(`
     <div>
-        <form method="POST">
-            <input name="email" placeholder='email' />
-            <input name="password" placeholder='password' />
-            <input name="passwordConfirmation" placeholder='password confirmation' />
-            <button>Sign Up</button>
-        </form>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
     </div>
   `);
 });
 
 app.post("/", (req, res) => {
-  res.send("Account created");
+  console.log(req.body);
+  res.send("Account created!!!");
 });
 
 app.listen(3000, () => {
-  console.log("listening");
+  console.log("Listening");
 });
